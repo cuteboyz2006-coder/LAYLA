a2db21b9bfb89f32d75c1abc3f0bd7be3a47199f
cuteboyz2006-coder <cuteboyz2006@gmail.com>
2026-09-26 00:07:58 +0530

    Implement text encoding and decoding with tokenizer

--- Main.py
+++ Main.py
@@ -1,1 +1,25 @@
+from brain.data import TextData
+from brain.tokenizer import Tokenizer
 
+
+data = TextData()
+
+data.add("hello Layla")
+data.add("Layla is learning")
+data.add("I like Python")
+
+
+tokenizer = Tokenizer()
+
+tokenizer.build_vocab(data.get_all())
+
+
+text = "hello Layla"
+
+encoded = tokenizer.encode(text)
+decoded = tokenizer.decode(encoded)
+
+
+print("Text:", text)
+print("Encoded:", encoded)
+print("Decoded:", decoded)
