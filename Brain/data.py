ee8798279b5dbfb9e78edfcc5b01fe2cd325f716
cuteboyz2006-coder <cuteboyz2006@gmail.com>
2026-09-26 00:28:52 +0530

    Implement make_sequences method for data processing
    Added a method to create input-target sequences from the data using a tokenizer.

--- Brain/data.py
+++ Brain/data.py
@@ -10,3 +10,28 @@ class TextData:
 
     def get_all(self):
         return self.data
+
+    def make_sequences(self, tokenizer, sequence_length=8):
+        sequences = []
+
+        for text in self.data:
+            token_ids = tokenizer.encode(text)
+
+            if len(token_ids) < 2:
+                continue
+
+            for i in range(len(token_ids) - 1):
+                input_ids = token_ids[:i + 1]
+                target_ids = token_ids[1:i + 2]
+
+                if len(input_ids) > sequence_length:
+                    break
+
+                sequences.append(
+                    {
+                        "input": input_ids,
+                        "target": target_ids,
+                    }
+                )
+
+        return sequences
