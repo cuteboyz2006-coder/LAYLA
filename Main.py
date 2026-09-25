5a568694c452ff6216b183c974a69ce559645047
cuteboyz2006-coder <cuteboyz2006@gmail.com>
2026-09-26 00:27:00 +0530

    Capitalize 'Hello' and enhance output details
    Updated text and data entries to capitalize 'Hello'. Added vocabulary size and content print statements.

--- Main.py
+++ Main.py
@@ -1,11 +1,13 @@
 from Brain.data import TextData
 from Brain.tokenizer import Tokenizer
 
+
 data = TextData()
 
-data.add("hello Layla")
+data.add("Hello Layla")
 data.add("Layla is learning")
 data.add("I like Python")
+data.add("Layla can learn from data")
 
 
 tokenizer = Tokenizer()
@@ -13,12 +15,15 @@ tokenizer = Tokenizer()
 tokenizer.build_vocab(data.get_all())
 
 
-text = "hello Layla"
+text = "Hello Layla"
 
 encoded = tokenizer.encode(text)
 decoded = tokenizer.decode(encoded)
 
 
-print("Text:", text)
+print("Original:", text)
 print("Encoded:", encoded)
 print("Decoded:", decoded)
+
+print("Vocabulary size:", len(tokenizer.token_to_id))
+print("Vocabulary:", tokenizer.token_to_id)
