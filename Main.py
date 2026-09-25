8a15b284f9e0618297d37f0cf854a86bec922242
cuteboyz2006-coder <cuteboyz2006@gmail.com>
2026-09-26 00:42:45 +0530

    Add SelfAttention to process embedding vectors

--- Main.py
+++ Main.py
@@ -1,6 +1,7 @@
 from Brain.data import TextData
 from Brain.tokenizer import Tokenizer
 from Brain.embeddings import TokenEmbeddings
+from Brain.attention import SelfAttention
 
 
 data = TextData()
@@ -12,11 +13,17 @@ data.add("Layla can learn from data")
 
 
 tokenizer = Tokenizer()
+
 tokenizer.build_vocab(data.get_all())
 
 
 embedding = TokenEmbeddings(
     vocab_size=len(tokenizer.token_to_id),
+    embedding_size=16
+)
+
+
+attention = SelfAttention(
     embedding_size=16
 )
 
@@ -27,15 +34,22 @@ token_ids = tokenizer.encode(text)
 
 vectors = embedding.encode(token_ids)
 
+attention_output = attention.forward(vectors)
+
 
 print("Text:", text)
 print("Token IDs:", token_ids)
 print("Embedding size:", len(vectors[0]))
+print("Attention output tokens:", len(attention_output))
+print(
+    "Attention vector size:",
+    len(attention_output[0])
+)
 
-for token_id, vector in zip(token_ids, vectors):
+for token_id, output in zip(token_ids, attention_output):
     print(
         "Token:",
         tokenizer.id_to_token[token_id],
-        "Vector:",
-        vector
+        "Attention:",
+        output
     )
